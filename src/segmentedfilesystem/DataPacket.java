59ff09c78b4b10e8c126c23f2b33013e141d2cda
carri320 <carri320@morris.umn.edu>
2020-10-29 10:34:53 -0500

    Added more to DataPacket.java

--- src/segmentedfilesystem/DataPacket.java
+++ src/segmentedfilesystem/DataPacket.java
@@ -3,16 +3,29 @@ import java.util.Arrays;
 import java.net.DatagramPacket;
 
 public class DataPacket {
-    byte[] status;
-    byte[] ID;
-    byte[] packNum;
+    String stat = "data"; 
+    byte status;
     byte[] data;
+    int packNum;
+    byte ID;
+    byte[] info;
+    boolean added;
+    
     public DataPacket(DatagramPacket packet){
-        this.status = packet.getData();
-        this.ID = ID;
-        this.packNum = packNum;
-        this.data = data;
-
-
+        this.data = packet.getData();
+        this.setPackNum();
+	this.ID = data[1];
+	this.status = data[0];
+	int length = packet.getLength();
+	this.info = Arrays.copyOfRange(packet.getData(), 4, length);
+	this.added = false;
+    }
+    public void setPackNum() {
+	int x = Byte.toUnsignedInt(data[2]);
+	int y = Byte.toUnsignedInt(data[3]);
+	this.packNum = (256 * x + y);
+    }
+    public byte getID() {
+         return ID;
     }
 }
