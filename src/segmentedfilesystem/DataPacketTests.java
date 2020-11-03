33aea59ac6b6f02c29c8fdbad6ee2bc3a8d3f3b1
tyler <rowla070@morris.umn.edu>
2020-11-03 10:23:45 -0600

    Create framework for testing DataPacket

---
+++ src/segmentedfilesystem/DataPacketTests.java
@@ -0,0 +1,30 @@
+package segmentedfilesystem;
+
+import junit.framework.*;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class DataPacketTests {
+    public static void main(String [] args){
+
+    }
+    @Test
+    public void testFileID() {
+
+    }
+    @Test
+    public void testPacketNum(){
+
+    }
+    @Test
+    public void testStatusByte(){
+
+    }
+    @Test
+    public void testData(){
+
+    }
+
+
+}
