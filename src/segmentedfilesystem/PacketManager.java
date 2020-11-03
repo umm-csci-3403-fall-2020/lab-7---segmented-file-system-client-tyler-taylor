5ed9a5dcdb436c603df061e6d05af34c5dc38cc8
carri320 <46938654+carri320@users.noreply.github.com>
2020-11-03 10:12:46 -0600

    Update PacketManager.java
    Got a start on PacketManager

--- src/segmentedfilesystem/PacketManager.java
+++ src/segmentedfilesystem/PacketManager.java
@@ -1,15 +1,34 @@
 package segmentedfilesystem;
-
-import java.net.DatagramPacket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PacketManager {
+    boolean full;
+    List<DataPacket> dataPacketList;
+    int maxSize;
+    byte ID;
 
-    public PacketManager(DataPacket pack){
+    public PacketManager(byte ID) {
+        this.dataPacketList = new ArrayList<DataPacket>();
+        this.full = false;
+        this.maxSize = 100000;
+        this.ID = ID;
 
     }
 
-    public void allPacketsRecieved(){
+    public void allPacketsRecieved(DataPacket dpacket){
+        dataPacketList.add(dpacket);
+        if (dataPacketList.size() == this.maxSize){
+            this.full = true;
+        }
 
     }
 
+    public void setID(byte ID) {
+        this.ID = ID;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
 }
